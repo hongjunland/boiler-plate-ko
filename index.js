cd3be2203c95798e474214241a2e8552fc9875ed
zunza96 <zunza96@gmail.com>
2021-04-06 19:23:06 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:root@education.d1tcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const bodyparser = require('body-parser');
+const config = require('./config/key')
+const {User} = require('./models/User');
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(bodyparser.json());
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false,
 }).then(()=>console.log('MongoDB Connected....'))
-.catch(err=>console.error(err))
-app.get('/',(res,req)=> res.send("Hello World"));
+.catch(err=>console.error(err));
+app.get('/',(req,res)=> res.send("Hello World!2222!"));
+
+app.post('/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err,doc)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
+
+})
 
 app.listen(port,()=>console.log(`Example app listsd on port ${port}!`));
